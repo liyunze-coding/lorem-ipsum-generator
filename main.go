2d683432cb921be192f650f69491a08f93b939b4
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2024-11-10 09:29:18 +1100

    improved Go functions

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-// main.go
 package main
 
 import (
@@ -21,66 +20,76 @@ type Response struct {
 	Text string `json:"text"`
 }
 
-func main() {
+func loadEnv() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
 
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	pageData := PageData{
+		Title: "Lorem Ipsum Generator",
+	}
+
+	renderTemplate(w, "index.html", pageData)
+}
+
+func handleLoremIpsum(w http.ResponseWriter, r *http.Request) {
 	API_KEY := os.Getenv("API_KEY")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		pageData := PageData{
-			Title: "Lorem Ipsum Generator",
-		}
+	// get data from form
+	r.ParseForm()
+	maxChar := r.PostFormValue("max-char")
+	maxCharEnable := r.FormValue("max-char-bool")
+	paragraphs := r.PostFormValue("paragraphs")
 
-		renderTemplate(w, "index.html", pageData)
-	})
+	var apiURL string
 
-	http.HandleFunc("/get-lorem-ipsum", func(w http.ResponseWriter, r *http.Request) {
-		// get data from form
-		r.ParseForm()
-		maxChar := r.PostFormValue("max-char")
-		maxCharEnable := r.FormValue("max-char-bool")
-		paragraphs := r.PostFormValue("paragraphs")
+	if maxCharEnable == "on" {
+		apiURL = fmt.Sprintf("https://api.api-ninjas.com/v1/loremipsum?max_length=%s&paragraphs=%s", maxChar, paragraphs)
+	} else {
+		apiURL = fmt.Sprintf("https://api.api-ninjas.com/v1/loremipsum?paragraphs=%s", paragraphs)
+	}
 
-		var apiURL string
+	result, err := makeAPIRequest(apiURL, API_KEY)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-		if maxCharEnable == "on" {
-			apiURL = fmt.Sprintf("https://api.api-ninjas.com/v1/loremipsum?max_length=%s&paragraphs=%s", maxChar, paragraphs)
-		} else {
-			apiURL = fmt.Sprintf("https://api.api-ninjas.com/v1/loremipsum?paragraphs=%s", paragraphs)
-		}
+	fmt.Fprint(w, result.Text)
+}
 
-		// GET request
-		req, err := http.NewRequest("GET", apiURL, nil)
+func makeAPIRequest(apiURL, apiKey string) (*Response, error) {
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		return nil, err
+	}
 
-		if err != nil {
-			log.Fatalln(err)
-		}
+	// Set API key in header
+	req.Header.Set("X-Api-Key", apiKey)
 
-		// Set API key in header
-		req.Header.Set("X-Api-Key", API_KEY)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-		client := &http.Client{}
+	var result Response
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
 
-		resp, err := client.Do(req)
+	return &result, nil
+}
 
-		if err != nil {
-			log.Fatalln(err)
-		}
+func main() {
+	loadEnv()
 
-		defer resp.Body.Close()
-
-		var result Response
-
-		err = json.NewDecoder(resp.Body).Decode(&result)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		fmt.Fprint(w, result.Text)
-	})
-
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/get-lorem-ipsum", handleLoremIpsum)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	fmt.Println("Server is running on http://localhost:8008")
